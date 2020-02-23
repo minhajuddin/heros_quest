12a9f5bbd10f340e9eef8fb98d825d0c2e889102
Khaja Minhajuddin <minhajuddin.k@gmail.com>
2020-02-23 01:21:19 -0500

    Color the board

--- lib/chess_board_web/views/game_live.ex
+++ lib/chess_board_web/views/game_live.ex
@@ -8,7 +8,7 @@ defmodule ChessBoardWeb.GameLive do
     <%= @duration %> <br />
       <div class="board">
         <%= for y <- 0..(@rows-1), x <- 0..(@cols-1) do %>
-          <div class="col" style="background-color: blue">
+          <div class="col" style="background-color: <%= color({x, y}, @my_coords, @player_coords[{x, y}]) %>">
             <%= "{#{x}, #{y}}" %> <%= @player_coords[{x, y}] %>
           </div>
         <% end %>
@@ -20,6 +20,13 @@ defmodule ChessBoardWeb.GameLive do
     <button phx-click=player-move phx-value-direction="right">right</button>
     """
   end
+
+  @empty_cell_color "#ffffff"
+  @my_color "#2ECC40"
+  @other_players_color "#FF851B"
+  defp color(_coords, _my_coords, nil), do: @empty_cell_color
+  defp color(coords, coords, _), do: @my_color
+  defp color(_, _, _), do: @other_players_color
 
   def mount(params, %{}, socket) do
     player_pid = Game.find_or_create_player(params["name"] || random_name())
@@ -51,15 +58,15 @@ defmodule ChessBoardWeb.GameLive do
 
     {{rows, cols}, player_coords} = Game.layout()
 
-    socket
-    |> assign(:current_time, current_time)
-    |> assign(:duration, duration(socket.assigns.start_time))
-    |> assign(:player_coords, player_coords)
-    |> assign(:rows, rows)
-    |> assign(:cols, cols)
+    assign(socket,
+      cols: cols,
+      rows: rows,
+      player_coords: player_coords,
+      my_coords: Player.get_coords(socket.assigns.player_pid),
+      duration: duration(socket.assigns.start_time),
+      current_time: current_time
+    )
   end
-
-  defp color(_time), do: Enum.random(~w[red black white blue green])
 
   defp duration(start_time) do
     duration = DateTime.diff(DateTime.utc_now(), start_time)
